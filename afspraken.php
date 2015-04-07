b1e2af010c48613e28e76dc6ab32c2e5c2fd4c9a
aranna00 <aranna00@gmail.com>
2015-04-07 14:48:40 +0200

    kapvergunning list and afspraken list now tested before listed

--- afspraken.php
+++ afspraken.php
@@ -30,32 +30,36 @@ $error = array(1=> "niet alles is ingevuld","query is mislukt" )
 			?>
 		</div>
 		<?php if(!isset($_GET["new"])): ?>
-		<table border=1>
-			<tr>
-				<td>ID</td>
-				<td>naam</td>
-				<td>vraag</td>
-				<td>datum</td>
-			</tr>
-				<?php
-					foreach($test as $afspraak): ?>
-						<tr>
-						<td><?= $afspraak->ID ?></td>
-						<td><?= $afspraak->USER ?></td>
-						<td><?= $afspraak->REDEN ?></td>
-						<td><?= $afspraak->DATUM ?></td>
-						</tr>
-					<?php endforeach ?>
-		</table>
+			<?php if($test!==false): ?>
+				<table border=1>
+					<tr>
+						<td>ID</td>
+						<td>naam</td>
+						<td>vraag</td>
+						<td>datum</td>
+					</tr>
+						<?php
+							foreach($test as $afspraak): ?>
+								<tr>
+								<td><?= $afspraak->ID ?></td>
+								<td><?= $afspraak->USER ?></td>
+								<td><?= $afspraak->REDEN ?></td>
+								<td><?= $afspraak->DATUM ?></td>
+								</tr>
+							<?php endforeach ?>
+				</table>
+			<?php else: ?>
+				<p>U heeft nog geen afspraken gemaakt.</p>
+			<?php endif ?>
 		<?php endif; ?>
 		<?php
 		if(isset($_GET["new"])): ?>
 			<div id="afspraak">
-				<form id="afpr" method="POST" action="Send.php">
+				<form id="afpr" method="POST" action="send.php">
 					<input type="hidden" name="username" value="<?php echo $username; ?>" class="afpr"><br>
 					<input type="hidden" name="userid" value="<?php echo $_SESSION['id']; ?>" class="afpr"><br>
 					<TEXTAREA name="REDEN" placeholder="Plaats hier uw reden van het aanvragen van een afspraak." class="afpr"></TEXTAREA><br>
-					<input type="submit" class="blargh" value="aanvragen" class="afpr">
+					<input type="submit" class="blargh afpr" value="aanvragen">
 				</form>
 			</div>
 		<?php else: ?>
--- kapvergunning.php
+++ kapvergunning.php
@@ -31,32 +31,37 @@ $error=	array(1=> "een van de velden is niet ingevuld","de query is niet goed ge
 			?>
 		</div>
 		<?php if(!isset($_GET["new"])): ?>
-		<table border=1>
-			<tr>
-				<td>ID</td><td>naam</td><td>confirmed</td><td>accepted</td><td>comment</td>
-			</tr>
-				<?php
-				foreach($test as $vergunning){
-					echo "<tr>";
-					echo "<td> $vergunning->ID </td>";
-					echo "<td> $vergunning->USER </td>";
-					echo "<td> $vergunning->CONFIRMED </td>";
-					echo "<td> $vergunning->ACCEPTED </td>";
-					echo "<td> $vergunning->COMMENT </td>";
-					echo "</tr>";
-				}
-				?>
-		</table>
+			<?php if($test!==false): ?>
+				<table border=1>
+					<tr>
+						<td>ID</td><td>naam</td><td>confirmed</td><td>accepted</td><td>comment</td>
+					</tr>
+						<?php
+							/** @var array $test */
+							foreach($test as $vergunning){
+							echo "<tr>";
+							echo "<td> $vergunning->ID </td>";
+							echo "<td> $vergunning->USER </td>";
+							echo "<td> $vergunning->CONFIRMED </td>";
+							echo "<td> $vergunning->ACCEPTED </td>";
+							echo "<td> $vergunning->COMMENT </td>";
+							echo "</tr>";
+						}
+						?>
+				</table>
+			<?php else: ?>
+				<p>U heeft nog geen vergunningen aan gevraagt.</p>
+			<?php endif; ?>
 		<?php endif; ?>
 		<?php
 		if(isset($_GET["new"])): ?>
 			<div id="kapAanvraag">
-				<form id="Kap" method="POST" action="Send.php">
+				<form id="Kap" method="POST" action="send.php">
 					<input type="hidden" name="username" value="<?php echo $username; ?>" class="kap"><br>
 					<input type="hidden" name="userid" value="<?php echo $_SESSION['id']; ?>" class="kap"><br>
 					<TEXTAREA name="COMMENT" placeholder="Plaats hier uw reden van het aanvragen van een kapvergunning." class="kap"></TEXTAREA><br>
 					<input type="checkbox" name="spoed" value="1"> spoed<br>
-					<input type="submit" class="blargh" value="aanvragen" class="kap">
+					<input type="submit" class="blargh kap" value="aanvragen">
 				</form>
 			</div>
 		<?php else: ?>
